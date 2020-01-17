5e15b641f1b866500ca005269dc13ac0ea58558f
zaqxswx205 <786164498@qq.com>
2020-01-18 00:02:31 +0800

    first commit

---
+++ 阶乘最高项/untitled0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 23:59:03 2020
+
+@author: 78616
+"""
+
+import math
+
+def FirstNumber (n):
+    global Accumulator 
+    firstnumber = Accumulator*n
