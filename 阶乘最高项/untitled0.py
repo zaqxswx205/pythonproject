880fe551c119ab656cc42d40aafea1827e09035c
zaqxswx205 <786164498@qq.com>
2020-01-18 00:18:22 +0800

    first commit

--- 阶乘最高项/untitled0.py
+++ 阶乘最高项/untitled0.py
@@ -6,7 +6,20 @@ Created on Fri Jan 17 23:59:03 2020
 """
 
 import math
-
-def FirstNumber (n):
-    global Accumulator 
+import numpy
+def FirstNumber (n) :
+    global Accumulator
     firstnumber = Accumulator*n
+    while firstnumber >= 10 :
+        firstnumber = firstnumber/10
+    firstnumber = math.modf(firstnumber)[1]
+    Accumulator = firstnumber
+    return Accumulator
+def main() :
+    N = int(input())
+    firstnumberlist = numpy.array([int(FirstNumber(i)) for i in range(2,N+1)])
+    freq = numpy.bincount(firstnumberlist)
+    return freq
+if __name__ =='__main__' :
+    Accumulator = 1
+    FirstNumberFreq = main()
