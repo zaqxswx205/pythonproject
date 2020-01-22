ea8c06ba6ae21623bc0fcc516dbb6485bbca5f5d
zaqxswx205 <786164498@qq.com>
2020-01-22 22:46:17 +0800

    first

--- data/untitled2.py
+++ data/untitled2.py
@@ -23,40 +23,42 @@ class Plot(QMainWindow):
  
         gridLayout = QGridLayout(self)     
         centralWidget.setLayout(gridLayout)  
-        
+        self.framecon = [0, 1, 0, 0, 0, 0, 0]
         self.frame1 = QFrame(self)
-        self.frame1.resize(100,100)
-        self.frame1.setStyleSheet('background-color:red;')
         self.frame2 = QFrame(self)
-        self.frame2.resize(100,100)
-        self.frame2.setStyleSheet('background-color:green;')
         self.frame3 = QFrame(self)
-        self.frame3.resize(100,100)
-        self.frame3.setStyleSheet('background-color:red;')
         self.frame4 = QFrame(self)
-        self.frame4.resize(100,100)
+        self.frame5 = QFrame(self)
+        self.frame6 = QFrame(self)
+        self.frame7 = QFrame(self)
+        self.framelist = [self.frame1, self.frame2, self.frame3, self.frame4, self.frame5, self.frame6, self.frame7]
+        [i.resize(1000, 1000) for i in self.framelist]
+        self.framecon = [1, 0, 0, 0, 0, 0, 0]
+        self.frame1.setStyleSheet('background-color:red;')
+        self.frame2.setStyleSheet('background-color:blue;')
+        self.frame3.setStyleSheet('background-color:black;')
         self.frame4.setStyleSheet('background-color:green;')
-        self.frame1.setVisible(True)
-        self.frame2.setVisible(False)
+        self.frame5.setStyleSheet('background-color:red;')
+        self.frame6.setStyleSheet('background-color:green;')
+        self.frame7.setStyleSheet('background-color:blue;')
+        self.Plotmain()
+        self.framecon = [0, 1, 0, 0, 0, 0, 0]
         SMG1 = QAction('&寿命罐1', self)
-        SMG1.triggered.connect(self.PlotSMG1)
+        SMG2 = QAction('&寿命罐2', self)
+        SMG3 = QAction('&寿命罐3', self)
+        SMG4 = QAction('&寿命罐4', self)
+        SM = QAction('&扫描', self)
+        ZB = QAction('&中波', self)
+        DB = QAction('&短波', self)
+        self.qactionlist = [SMG1, SMG2, SMG3, SMG4, SM, ZB, DB]
+        [i.triggered.connect(self.Plotmain) for i in self.qactionlist]
         menu1 = self.menuBar().addMenu('寿命罐')
-        menu1.addAction(SMG1)
+        [menu1.addAction(self.qactionlist[i]) for i in range(4)]
         menu2 = self.menuBar().addMenu('载荷')
+        [menu2.addAction(self.qactionlist[i]) for i in range(4,7)]
 
-        '''     SMG2 = menu1.addAction('寿命罐2')
-        SMG3 = menu1.addAction('寿命罐3')
-        SMG4 = menu1.addAction('寿命罐4')
-        SM = menu2.addAction('扫描')
-        ZB = menu2.addAction('中波')
-        DB = menu2.addAction('短波')'''
-    def PlotSMG1(self):
-        self.frame1.setVisible(True)
-        self.frame2.setVisible(False)
- 
-    def on_pushButton_enter_clicked_1(self):
-        self.frame1.setVisible(False)
-        self.frame2.setVisible(True)
+    def Plotmain(self):
+        [self.framelist[i].setVisible(self.framecon[i]) for i in range(7)]
 if __name__ == "__main__":
     def run_app():
         app = QtWidgets.QApplication(sys.argv)
