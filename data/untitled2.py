291bd4d623db5d0816d5c35347f803dd07ed1948
zaqxswx205 <786164498@qq.com>
2020-02-02 12:56:51 +0800

    first

--- data/untitled2.py
+++ data/untitled2.py
@@ -6,63 +6,130 @@ Created on Tue Jan 14 10:56:49 2020
 """
 
 import sys
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget,QFrame,QAction
-from PyQt5.QtCore import QSize   
-from PyQt5.QtGui import QIcon 
+import PyQt5.QtWidgets
+import PyQt5.QtCore 
+import PyQt5.QtGui 
      
-class Plot(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
- 
-        self.setMinimumSize(QSize(1000,1000))    
-        self.setWindowTitle("Plot") 
+class MainWindow(PyQt5.QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs) :
+        super().__init__(*args, **kwargs)
+        self.setWindowTitle('PLOT')
+        self.showMaximized()
+        self.Layout = PyQt5.QtWidgets.QHBoxLayout()
+        self.centralWidget = PyQt5.QtWidgets.QWidget(self)          
+        self.setCentralWidget(self.centralWidget)   
+        self.stackedWidget = PyQt5.QtWidgets.QStackedWidget(self.centralWidget)
+        self.Layout.addWidget(self.stackedWidget)
+        self.TotalCategory = 5
+        self.Category = ['&寿命罐1', '&寿命罐2', '&扫描', '&短波', '&中波']
+        self.SMButton = ['制冷温度', '功率', '大端散热面', '小端散热面', '压缩机散热面']
+        self.ZBDBButton = ['制冷温度', '功率', '压缩机散热面', '脉管散热面']
+        self.StackConfiguration()
+        self.MenuConfiguration()
         
-        centralWidget = QWidget(self)          
-        self.setCentralWidget(centralWidget)   
- 
-        gridLayout = QGridLayout(self)     
-        centralWidget.setLayout(gridLayout)  
-        self.framecon = [0, 1, 0, 0, 0, 0, 0]
-        self.frame1 = QFrame(self)
-        self.frame2 = QFrame(self)
-        self.frame3 = QFrame(self)
-        self.frame4 = QFrame(self)
-        self.frame5 = QFrame(self)
-        self.frame6 = QFrame(self)
-        self.frame7 = QFrame(self)
-        self.framelist = [self.frame1, self.frame2, self.frame3, self.frame4, self.frame5, self.frame6, self.frame7]
-        [i.resize(1000, 1000) for i in self.framelist]
-        self.framecon = [1, 0, 0, 0, 0, 0, 0]
-        self.frame1.setStyleSheet('background-color:red;')
-        self.frame2.setStyleSheet('background-color:blue;')
-        self.frame3.setStyleSheet('background-color:black;')
-        self.frame4.setStyleSheet('background-color:green;')
-        self.frame5.setStyleSheet('background-color:red;')
-        self.frame6.setStyleSheet('background-color:green;')
-        self.frame7.setStyleSheet('background-color:blue;')
-        self.Plotmain()
-        self.framecon = [0, 1, 0, 0, 0, 0, 0]
-        SMG1 = QAction('&寿命罐1', self)
-        SMG2 = QAction('&寿命罐2', self)
-        SMG3 = QAction('&寿命罐3', self)
-        SMG4 = QAction('&寿命罐4', self)
-        SM = QAction('&扫描', self)
-        ZB = QAction('&中波', self)
-        DB = QAction('&短波', self)
-        self.qactionlist = [SMG1, SMG2, SMG3, SMG4, SM, ZB, DB]
-        [i.triggered.connect(self.Plotmain) for i in self.qactionlist]
-        menu1 = self.menuBar().addMenu('寿命罐')
-        [menu1.addAction(self.qactionlist[i]) for i in range(4)]
-        menu2 = self.menuBar().addMenu('载荷')
-        [menu2.addAction(self.qactionlist[i]) for i in range(4,7)]
+    def MenuConfiguration (self) :
+        Menu1 = self.menuBar().addMenu('寿命罐')
+        self.qActionList = [PyQt5.QtWidgets.QAction(self.Category[i], self) for i in range(self.TotalCategory)]
+        [Menu1.addAction(self.qActionList[i]) for i in range(self.TotalCategory)]
+        [self.qActionList[i].triggered.connect(self.MenuConnectStack) for i in range(self.TotalCategory)]
 
-    def Plotmain(self):
-        [self.framelist[i].setVisible(self.framecon[i]) for i in range(7)]
+    def StackConfiguration (self) :
+        self.StackList = globals()
+        for i in range(self.TotalCategory) :
+            self.StackList['self.stack'+str(i)] = PyQt5.QtWidgets.QWidget()
+            self.stackedWidget.addWidget(self.StackList['self.stack'+str(i)])
+        self.Stack0UI()
+        self.Stack1UI()
+        self.Stack2UI()
+        self.Stack3UI()
+        self.Stack4UI()
+
+    def MenuConnectStack(self) :
+        SenderPosition = self.Category.index(self.sender().text())
+        self.stackedWidget.setCurrentIndex(SenderPosition)
+
+    def Stack0UI(self):
+        Stack0 = PyQt5.QtWidgets.QHBoxLayout()
+        [Stack0.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.SMButton]
+        self.StackList['self.stack'+str(0)].setLayout(Stack0)
+
+    def Stack1UI(self):
+        Stack1 = PyQt5.QtWidgets.QVBoxLayout()
+        ButtonLayout1 = PyQt5.QtWidgets.QHBoxLayout()
+        ButtonLayout2 = PyQt5.QtWidgets.QHBoxLayout()
+        [ButtonLayout1.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.SMButton]
+        [ButtonLayout2.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.SMButton]
+        Stack1.addLayout(ButtonLayout1)
+        Stack1.addLayout(ButtonLayout2)
+        self.StackList['self.stack'+str(1)].setLayout(Stack1)
+
+    def Stack2UI(self):
+        Stack2 = PyQt5.QtWidgets.QHBoxLayout()
+        self.TabWidget = PyQt5.QtWidgets.QTabWidget(self.StackList['self.stack'+str(2)])
+        self.tab1 = PyQt5.QtWidgets.QWidget()
+        self.tab2 = PyQt5.QtWidgets.QWidget()
+        self.tab3 = PyQt5.QtWidgets.QWidget()
+        self.TabWidget.addTab(self.tab1, "扫描 1")
+        self.TabWidget.addTab(self.tab2, "扫描 2")
+        self.TabWidget.addTab(self.tab3, "扫描 3")
+        self.Tabcat1 = PyQt5.QtWidgets.QHBoxLayout()
+        self.Tabcat2 = PyQt5.QtWidgets.QHBoxLayout()
+        self.Tabcat3 = PyQt5.QtWidgets.QHBoxLayout()
+        [self.Tabcat1.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.SMButton]
+        [self.Tabcat2.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.SMButton]
+        [self.Tabcat3.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.SMButton]
+        self.tab1.setLayout(self.Tabcat1)
+        self.tab2.setLayout(self.Tabcat2)
+        self.tab3.setLayout(self.Tabcat3)
+        Stack2.addWidget(self.TabWidget)
+        self.StackList['self.stack'+str(2)].setLayout(Stack2)
+
+    def Stack3UI(self):
+        Stack3 = PyQt5.QtWidgets.QHBoxLayout()
+        self.TabWidget = PyQt5.QtWidgets.QTabWidget(self.StackList['self.stack'+str(3)])
+        self.tab1 = PyQt5.QtWidgets.QWidget()
+        self.tab2 = PyQt5.QtWidgets.QWidget()
+        self.tab3 = PyQt5.QtWidgets.QWidget()
+        self.TabWidget.addTab(self.tab1, "扫描 1")
+        self.TabWidget.addTab(self.tab2, "扫描 2")
+        self.TabWidget.addTab(self.tab3, "扫描 3")
+        self.Tabcat1 = PyQt5.QtWidgets.QHBoxLayout()
+        self.Tabcat2 = PyQt5.QtWidgets.QHBoxLayout()
+        self.Tabcat3 = PyQt5.QtWidgets.QHBoxLayout()
+        [self.Tabcat1.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.ZBDBButton]
+        [self.Tabcat2.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.ZBDBButton]
+        [self.Tabcat3.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.ZBDBButton]
+        self.tab1.setLayout(self.Tabcat1)
+        self.tab2.setLayout(self.Tabcat2)
+        self.tab3.setLayout(self.Tabcat3)
+        Stack3.addWidget(self.TabWidget)
+        self.StackList['self.stack'+str(3)].setLayout(Stack3)
+
+    def Stack4UI(self):
+        Stack4 = PyQt5.QtWidgets.QHBoxLayout()
+        self.TabWidget = PyQt5.QtWidgets.QTabWidget(self.StackList['self.stack'+str(4)])
+        self.tab1 = PyQt5.QtWidgets.QWidget()
+        self.tab2 = PyQt5.QtWidgets.QWidget()
+        self.tab3 = PyQt5.QtWidgets.QWidget()
+        self.TabWidget.addTab(self.tab1, "扫描 1")
+        self.TabWidget.addTab(self.tab2, "扫描 2")
+        self.TabWidget.addTab(self.tab3, "扫描 3")
+        self.Tabcat1 = PyQt5.QtWidgets.QHBoxLayout()
+        self.Tabcat2 = PyQt5.QtWidgets.QHBoxLayout()
+        self.Tabcat3 = PyQt5.QtWidgets.QHBoxLayout()
+        [self.Tabcat1.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.ZBDBButton]
+        [self.Tabcat2.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.ZBDBButton]
+        [self.Tabcat3.addWidget(PyQt5.QtWidgets.QRadioButton(i)) for i in self.ZBDBButton]
+        self.tab1.setLayout(self.Tabcat1)
+        self.tab2.setLayout(self.Tabcat2)
+        self.tab3.setLayout(self.Tabcat3)
+        Stack4.addWidget(self.TabWidget)
+        self.StackList['self.stack'+str(4)].setLayout(Stack4)
+
+
+    
 if __name__ == "__main__":
-    def run_app():
-        app = QtWidgets.QApplication(sys.argv)
-        mainWin = Plot()
-        mainWin.show()
-        app.exec_()
-    run_app()
+    app = PyQt5.QtWidgets.QApplication(sys.argv)
+    the_mainwindow = MainWindow()
+    the_mainwindow.show()
+    sys.exit(app.exec_())
